d4ce76646415576178beb63e38f62c3e37ec4f3d
Simone Tellini <simone@tellini.info>
2021-03-14 12:58:06 +0100

    removed mostly useless browser detections. I'd consider the whole Prototype.Browser collection deprecated anyway...

--- src/prototype/prototype.js
+++ src/prototype/prototype.js
@@ -61,12 +61,11 @@ var Prototype = {
   **/
   Version: '<%= PROTOTYPE_VERSION %>',
 
-  /**
+  /** deprecated
    *  Prototype.Browser
    *
    *  A collection of [[Boolean]] values indicating the browser which is
-   *  currently in use. Available properties are `IE`, `Opera`, `WebKit`,
-   *  `MobileSafari` and `Gecko`.
+   *  currently in use. Available properties `WebKit`.
    *
    *  Example
    *
@@ -74,16 +73,9 @@ var Prototype = {
    *      //-> true, when executed in any WebKit-based browser.
   **/
   Browser: (function(){
-    var ua = navigator.userAgent;
-    // Opera (at least) 8.x+ has "Opera" as a [[Class]] of `window.opera`
-    // This is a safer inference than plain boolean type conversion of `window.opera`
-    var isOpera = Object.prototype.toString.call(window.opera) == '[object Opera]';
+    const ua = navigator.userAgent;
     return {
-      IE:             !!window.attachEvent && !isOpera,
-      Opera:          isOpera,
-      WebKit:         ua.indexOf('AppleWebKit/') > -1,
-      Gecko:          ua.indexOf('Gecko') > -1 && ua.indexOf('KHTML') === -1,
-      MobileSafari:   /Apple.*Mobile/.test(ua)
+      WebKit:         ua.indexOf('AppleWebKit/') > -1
     }
   })(),
 
